cee92d59c22437a3ba8ecf673b431f3a69945883
markus.moeller <markus.moeller@WINAPIVO21SGC8U>
2023-02-13 18:07:37 +0100

    Add project files.

---
+++ TabSSOGraphUploadPDF/Controllers/UploadController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.Graph;
+using Microsoft.Identity.Web;
+using System.IO;
+using System.Net;
+using TabSSOGraphUploadPDF.Models;
+
+namespace TabSSOGraphUploadPDF.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UploadController : ControllerBase
+    {
+        private readonly GraphServiceClient _graphClient;
+        private readonly ITokenAcquisition _tokenAcquisition;
+        private readonly ILogger<UploadController> _logger;
+        public UploadController(ITokenAcquisition tokenAcquisition, GraphServiceClient graphClient, ILogger<UploadController> logger)
+        {
+            _tokenAcquisition = tokenAcquisition;
+            _graphClient = graphClient;
+            _logger = logger;
+        }
+        // api/<controller>/GetMimeMessage
+        [HttpPost]
+        public async Task<ActionResult<string>> Post([FromForm] UploadRequest fileUpload)
+        {
+            string accessToken = await GetAccessToken();
+
+            //IFormFile file = Request.Form.Files.FirstOrDefault();
+            string fileName = fileUpload.Name;
+            string siteUrl = fileUpload.SiteUrl;
+            _logger.LogInformation($"Received file {fileUpload.file.FileName} with size in bytes {fileUpload.file.Length}");
+            string userID = User.GetObjectId(); //   Claims["preferred_username"];
+            DriveItem uploadResult = await this._graphClient.Users[userID]
+                                                    .Drive.Root
+                                                    .ItemWithPath(fileUpload.file.FileName)
+                                                    .Content.Request()
+                                                    .PutAsync<DriveItem>(fileUpload.file.OpenReadStream());
+            return Ok("{'file'}");
+            //return Ok(uploadResult.WebUrl);
+        }
+
+        private async Task<string> GetAccessToken()
+        {
+            _logger.LogInformation($"Authenticated user: {User.GetDisplayName()}");
+
+            try
+            {
+                // TEMPORARY
+                // Get a Graph token via OBO flow
+                var token = await _tokenAcquisition
+                    .GetAccessTokenForUserAsync(new[]{
+                        "Files.ReadWrite", "Sites.ReadWrite.All" });
+
+                // Log the token
+                _logger.LogInformation($"Access token for Graph: {token}");
+                return token;
+            }
+            catch (MicrosoftIdentityWebChallengeUserException ex)
+            {
+                _logger.LogError(ex, "Consent required");
+                // This exception indicates consent is required.
+                // Return a 403 with "consent_required" in the body
+                // to signal to the tab it needs to prompt for consent
+                return "";
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error occurred");
+                return "";
+            }
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class DisableFormValueModelBindingAttribute : Attribute, IResourceFilter
+    {
+        public void OnResourceExecuting(ResourceExecutingContext context)
+        {
+            var factories = context.ValueProviderFactories;
+            factories.RemoveType<FormFileValueProviderFactory>();
+            factories.RemoveType<FormValueProviderFactory>();
+            factories.RemoveType<JQueryFormValueProviderFactory>();
+        }
+
+        public void OnResourceExecuted(ResourceExecutedContext context)
+        {
+        }
+    }
+}
---
+++ TabSSOGraphUploadPDF/wwwroot/js/TabPDF.js
@@ -0,0 +1,81 @@
+﻿(function (TabPDF, $, undefined) {
+  ssoToken = "";
+  siteUrl = "";
+
+  TabPDF.allowDrop = function (event) {
+    event.preventDefault();
+    event.stopPropagation();
+    event.dataTransfer.dropEffect = 'copy';
+  }
+
+  TabPDF.getSSOToken = function () {
+    if (microsoftTeams) {
+      microsoftTeams.initialize();
+      microsoftTeams.authentication.getAuthToken({
+        successCallback: (token, event) => {
+          console.log(token);
+          TabPDF.ssoToken = token;
+          
+        },
+        failureCallback: (error) => {
+          renderError(error);
+        }
+      });
+    }
+  }
+
+  TabPDF.getContext = function () {
+    if (microsoftTeams) {
+      microsoftTeams.app.getContext()
+        .then(context => {
+          if (context.sharePointSite.teamSiteUrl !== "") {
+            TabPDF.siteUrl = context.sharePointSite.teamSiteUrl;
+          }
+          else {
+            TabPDF.siteUrl = "https://" + context.sharePointSite.teamSiteDomain;
+          }
+        });
+    }
+  }
+
+  TabPDF.executeUpload = function (event) {
+    TabPDF.allowDrop(event);
+    const dt = event.dataTransfer;
+    const files = Array.prototype.slice.call(dt.files); // [...dt.files];
+    files.forEach(fileToUpload => {
+      //if (Utilities.validFileExtension(fileToUpload.name)) {
+      //alert("File " + fileToUpload.name + " Uploaded");
+      const formData = new FormData();
+      formData.append('file', fileToUpload);
+      formData.append('Name', fileToUpload.name);
+      formData.append('SiteUrl', TabPDF.siteUrl);
+      //const item = {
+      //  Name: fileToUpload.name,
+      //  SiteUrl: TabPDF.siteUrl
+      //};
+      fetch("/api/Upload", {
+        method: "post",
+        headers: {
+          "Authorization": "Bearer " + TabPDF.ssoToken
+          //"Content-Type": "multipart/form-data; boundary=--WebKitFormBoundaryfgtsKTYLsT7PNUVD",
+          // "Content-Type": "x-www-form-urlencoded"
+          // "Content-Type": "application/json"
+          // "Content-Length": fileToUpload.size
+        },
+        body: formData // JSON.stringify(item)
+      })
+      .then((response) => {
+        response.json().then(resp => {
+          console.log(resp);
+        });
+      });
+      //}
+    });
+  }
+
+  /// Class 'user' for TabPDF
+  TabPDF.Drag = {};
+  {
+
+  }
+}(window.TabPDF = window.TabPDF || {}));
